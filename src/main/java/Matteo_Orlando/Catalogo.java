8bfca016abcd675dbb8096c00144afc73e5ad4a3
MatteoOrlando <mat.orlando88@gmail.com>
2024-03-22 15:02:05 +0100

    Update Catalogo.java

--- src/main/java/Matteo_Orlando/Catalogo.java
+++ src/main/java/Matteo_Orlando/Catalogo.java
@@ -3,9 +3,8 @@ package Matteo_Orlando;
 import entities_model.Libri;
 import entities_model.Utente;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import javax.persistence.*;
+import java.util.List;
 
 public class Catalogo {
     public static void main(String[] args) {
@@ -16,6 +15,10 @@ public class Catalogo {
 
         entityManager.getTransaction().begin();
 
+        aggiungiUtente(entityManager, "Giovanni", "Storti", "1954-02-10", "UT004");
+        aggiungiUtente(entityManager, "Renato", "Zero", "1943-10-23", "UT005");
+
+        rimuoviLibroPerISBN(entityManager, "9788804487902");
 
         Utente utente1 = new Utente();
         utente1.setNome("Mario");
@@ -31,15 +34,45 @@ public class Catalogo {
         utente2.setNumeroTessera("UT002");
         entityManager.persist(utente2);
 
+        Utente utente3 = new Utente();
+        utente3.setNome("Gianni");
+        utente3.setCognome("Neri");
+        utente3.setDataDiNascita("1965-12-16");
+        utente3.setNumeroTessera("UT003");
+        entityManager.persist(utente3);
+        entityManager.refresh(utente3);
+
 
         Libri libro1 = new Libri();
-        libro1.setTitolo("Il signore degli anelli");
+        libro1.setTitolo("Harry Potter e la pietra Filosofale");
         libro1.setISBN("9788804487902");
-        libro1.setAnnoPubblicazione(1954);
-        libro1.setNumeroPagine(1170);
-        libro1.setAutore("J.R.R. Tolkien");
+        libro1.setAnnoPubblicazione(1997);
+        libro1.setNumeroPagine(304);
+        libro1.setAutore("J.K. Rowling");
         libro1.setGenere("Fantasy");
         entityManager.persist(libro1);
+
+        Libri libro2 = new Libri();
+        libro2.setTitolo("Harry Potter e la camera dei segreti");
+        libro2.setISBN("9788804487903");
+        libro2.setAnnoPubblicazione(1998);
+        libro2.setNumeroPagine(336);
+        libro2.setAutore("J.K. Rowling");
+        libro2.setGenere("Fantasy");
+        entityManager.persist(libro2);
+
+        String ISBN = "9788804487903";
+        List<Libri> risultatiRicerca = ricercaPerISBN(entityManager, ISBN);
+        if (!risultatiRicerca.isEmpty()) {
+            System.out.println("Libro trovato:");
+            for (Libri libro : risultatiRicerca) {
+                System.out.println("Titolo: " + libro.getTitolo());
+                System.out.println("Autore: " + libro.getAutore());
+
+            }
+        } else {
+            System.out.println("Nessun libro trovato per l'ISBN: " + ISBN);
+        }
         
         
         entityManager.getTransaction().commit();
@@ -47,6 +80,7 @@ public class Catalogo {
         entityManager.close();
         entityManagerFactory.close();
     }
+
     
     // Metodo per aggiungere un utente 
     private static void aggiungiUtente(EntityManager entityManager, String nome, String cognome, String dataDiNascita, String numeroTessera) {
@@ -67,7 +101,27 @@ public class Catalogo {
             System.out.println("Libro non trovato per ISBN: " + ISBN);
         }
     }
+    // Metodo per ricerca per ISBN
+    private static List<Libri> ricercaPerISBN(EntityManager entityManager, String ISBN) {
+        String jpql = "SELECT l FROM Libri l WHERE l.ISBN = :ISBN";
+        TypedQuery<Libri> query = entityManager.createQuery(jpql, Libri.class);
+        query.setParameter("ISBN", ISBN);
+        return query.getResultList();
+    }
 
+    // Metodo per ricerca per anno di pubblicazione
+    private static List ricercaPerAnnoPubblicazione(EntityManager entityManager, int anno) {
+        Query query = entityManager.createQuery("SELECT l FROM Libri l WHERE l.annoPubblicazione = :anno");
+        query.setParameter("anno", anno);
+        return query.getResultList();
+    }
+
+    // Metodo per ricerca per autore
+    private static List ricercaPerAutore(EntityManager entityManager, String autore) {
+        Query query = entityManager.createQuery("SELECT l FROM Libri l WHERE l.autore = :autore");
+        query.setParameter("autore", autore);
+        return query.getResultList();
+    }
     
 }
     
